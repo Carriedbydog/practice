43b0bfd49eee0a50ae162894d101f07493f28584
Vitalii Dimov <113688545+Carriedbydog@users.noreply.github.com>
2023-09-25 21:16:44 +0300

    init update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,52 @@
+import React, { useEffect, useState } from 'react';
+
+import { ContactList } from './Contacts/ContactList';
+import { Filter } from './Filter/Filter';
+import { nanoid } from 'nanoid';
+import { StyledSubTitle, StyledTitle, StyledWrapper } from 'styles/App.styled';
+import { Form } from './ContactForm/Form';
+
 export const App = () => {
+  const [contacts, setContacts] = useState(
+    JSON.parse(window.localStorage.getItem('contacts')) || []
+  );
+  const [filter, setFilter] = useState('');
+
+  useEffect(() => {
+    window.localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
+
+  const handleAddContact = (name, number) => {
+    const item = contacts.find(
+      item => item.name.toLowerCase() === name.toLowerCase()
+    );
+
+    if (item) {
+      alert(`${name} is already in contacts`);
+      return;
+    }
+
+    const contact = {
+      name,
+      number,
+      id: nanoid(),
+    };
+
+    setContacts(prev => [...prev, contact]);
+  };
+
+  const handleChangeFilter = e => {
+    setFilter(e.target.value);
+  };
+  const filterContacts = () => {
+    const normalizedFilter = filter.toLowerCase();
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(normalizedFilter)
+    );
+  };
+  const handleContactDelete = id => {
+    setContacts(prev => prev.filter(contact => contact.id !== id));
+  };
   return (
     <div
       style={{
@@ -7,10 +55,19 @@ export const App = () => {
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+        color: '#010101',
       }}
     >
-      React homework template
+      <StyledWrapper>
+        <StyledTitle>PhoneBook</StyledTitle>
+        <Form handleAddContact={handleAddContact} />
+        <StyledSubTitle>Contacts</StyledSubTitle>
+        <Filter inputFilterData={handleChangeFilter} filterValue={filter} />
+        <ContactList
+          contacts={filterContacts()}
+          onDelete={handleContactDelete}
+        />
+      </StyledWrapper>
     </div>
   );
 };
---
+++ src/components/ContactForm/Form.jsx
@@ -0,0 +1,51 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import { StyledButton, StyledInput, StyledLabel } from './Form.styled';
+
+export const Form = ({ handleAddContact }) => {
+  const [name, setName] = useState('');
+  const [number, setNumber] = useState('');
+
+  const handleFormSubmit = e => {
+    e.preventDefault();
+
+    handleAddContact(name, number);
+    setName('');
+    setNumber('');
+  };
+  return (
+    <form onSubmit={handleFormSubmit}>
+      <StyledLabel>
+        Name:
+        <StyledInput
+          type="text"
+          name="name"
+          value={name}
+          placeholder="Enter your name"
+          pattern="^([a-zA-Z]|\s)*$"
+          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+          required
+          onChange={e => setName(e.target.value)}
+        />
+      </StyledLabel>
+      <StyledLabel>
+        Tel:
+        <StyledInput
+          type="tel"
+          name="number"
+          placeholder="Enter your number"
+          pattern="^[0-9]+$"
+          title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+          required
+          value={number}
+          onChange={e => setNumber(e.target.value)}
+        />
+      </StyledLabel>
+      <StyledButton type="submit">Add contact</StyledButton>
+    </form>
+  );
+};
+
+Form.propTypes = {
+  handleAddContact: PropTypes.func,
+};
